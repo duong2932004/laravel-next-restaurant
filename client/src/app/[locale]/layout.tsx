1bc419087595deed685ca850023d45e64efa9e0d
Dương <duong2932004@gmail.com>
2024-12-22 19:34:01 +0700

    commit

--- client/src/app/[locale]/layout.tsx
+++ client/src/app/[locale]/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
-import Footer from "@/components/footer";
 import { ThemeProvider } from "@/components/dark-mode/theme-provider";
 import NextIntlProvider from "@/components/providers/next-intl-provider";
 import { Toaster } from "@/components/ui/toaster";
